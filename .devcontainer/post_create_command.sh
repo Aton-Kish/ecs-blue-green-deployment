4ff011ca45af48283d1321088b6e131e10d421a1
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-02-25 06:06:08 +0000

    chore: installed ecspresso

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -6,3 +6,5 @@ sudo apt-get update
 sudo apt-get install -y bash-completion
 
 echo -e "\ncomplete -C '/usr/local/bin/aws_completer' aws" >> ~/.bashrc
+
+go install github.com/kayac/ecspresso/v2/cmd/ecspresso@v2.3.2
